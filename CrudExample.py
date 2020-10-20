83f1814afd390b3955cc4e666648eb076d8df515
Mokytojas <aidas.puskunigis@kitm.lt>
2020-10-20 10:41:30 +0300

    Add CrudExample.py

---
+++ CrudExample.py
@@ -0,0 +1,9 @@
+from database import DatabaseContextManager
+
+"Foreign key table"
+Table = "Customers"
+Fields = [first_name, last_name, age]
+
+
+Table = "Companies"
+Fields = [company_name, employee_count, average_salary]
